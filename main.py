316228df0f634dee16bc8dcb3ecb0199f34bfe4a
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-11-15 22:34:52 -0800

    add colision check to game loop

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in asteroids:
+            if sprite.colide(player):
+                print("Game over")
+                pygame.QUIT
+                return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         # limit framerate to 60 FPS 
         dt = clock.tick(60) / 1000
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
